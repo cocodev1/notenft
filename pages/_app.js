6eef75ac36028d5611df3e62a96c3642a4b29e1b
simoncoco01 <simoncoco011@gmail.com>
2021-08-21 18:15:39 +0300

    Edit GA env path

--- pages/_app.js
+++ pages/_app.js
@@ -43,7 +43,7 @@ function MyApp({ Component, pageProps }) {
             window.dataLayer = window.dataLayer || [];
             function gtag(){dataLayer.push(arguments);}
             gtag('js', new Date());
-            gtag('config', '${process.env.GA_TRACKING_ID}', {
+            gtag('config', '${process.env.NEXT_PUBLIC_GA_ID}', {
               page_path: window.location.pathname,
             });
           `,
